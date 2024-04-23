81d33cb5df88dd59d31df954f6245c5e426c044c
Summer Long <salong@uchicago.edu>
2024-04-23 14:42:35 -0500

    Home page and prelim legal page

---
+++ frontend/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, Blueprint, render_template, redirect, url_for, request
+
+main = Blueprint('main', __name__) 
+
+@main.route('/')
+def home():
+    language = request.args.get('lang', 'en')
+    return render_template('home.html', language=language)
+
+@main.route('/legal')
+def legal():
+    language = request.args.get('lang', 'en')
+    return render_template('legal.html', language=language)
+
+app = Flask(__name__)
+
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
