292804d87836e53aa33457877531dd714484e438
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-24 23:35:00 -0500

    Adding Services and Hours db

--- new_arrivals_chi/app/database.py
+++ new_arrivals_chi/app/database.py
@@ -18,3 +18,36 @@ class Organization(db.model):
     phone = db.Column(db.String, nullable=False)
     image_path = db.Column(db.String, nullable=False)
     status = db.Column(db.String, nullable=False)
+
+
+class Language(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    org_id = db.Column(
+        db.Integer, db.ForeignKey("organization.id"), nullable=False
+    )
+    language = db.Column(db.String, nullable=False)
+
+
+class Hours:
+    id = db.Column(db.Integer, primary_key=True)
+    org_id = db.Column(
+        db.Integer, db.ForeignKey("organization.id"), nullable=False
+    )
+    day_of_week = db.Column(db.Integer, nullable=False)
+    opening_time = db.Column(db.Time, nullable=False)
+    closing_time = db.Column(db.Time, nullable=False)
+
+
+class Services:
+    id = db.Column(db.Integer, primary_key=True)
+    org_id = db.Column(
+        db.Integer, db.ForeignKey("organization.id"), nullable=False
+    )
+    location_id = db.Column(
+        db.Integer, db.ForeignKey("location.id"), nullable=False
+    )
+    date_id = db.Column(db.Integer, db.ForeignKey("date.id"), nullable=False)
+    category = db.Column(db.String, nullable=False)
+    service = db.Column(db.String, nullable=False)
+    access = db.Column(db.String, nullable=False)
+    service_note = db.Column(db.String, nullable=True)
