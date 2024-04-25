e086364f5c579687f123c3e47effa14c5e8711c0
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-25 17:01:49 -0500

    More adjustments to alembic script

--- new_arrivals_chi/app/database.py
+++ new_arrivals_chi/app/database.py
@@ -8,29 +8,22 @@ class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    location_id = db.Column(
-        db.Integer, db.ForeignKey("location.id"), nullable=False
-    )
+    location_id = db.Column(db.Integer, db.ForeignKey("location.id"), nullable=False)
     hours_id = db.Column(db.Integer, db.ForeignKey("hours.id"), nullable=False)
-    phone = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(25), nullable=False)
     image_path = db.Column(db.String(255), nullable=False)
     status = db.Column(db.String(50), nullable=False)
-    test_column = db.Column(db.String(100))  # Temporary test column
 
 
 class Language(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    org_id = db.Column(
-        db.Integer, db.ForeignKey("organization.id"), nullable=False
-    )
+    org_id = db.Column(db.Integer, db.ForeignKey("organization.id"), nullable=False)
     language = db.Column(db.String(50), nullable=False)
 
 
 class Hours(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    org_id = db.Column(
-        db.Integer, db.ForeignKey("organization.id"), nullable=False
-    )
+    org_id = db.Column(db.Integer, db.ForeignKey("organization.id"), nullable=False)
     day_of_week = db.Column(db.Integer, nullable=False)
     opening_time = db.Column(db.Time, nullable=False)
     closing_time = db.Column(db.Time, nullable=False)
@@ -38,12 +31,8 @@ class Hours(db.Model):
 
 class Services(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    org_id = db.Column(
-        db.Integer, db.ForeignKey("organization.id"), nullable=False
-    )
-    location_id = db.Column(
-        db.Integer, db.ForeignKey("location.id"), nullable=False
-    )
+    org_id = db.Column(db.Integer, db.ForeignKey("organization.id"), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("location.id"), nullable=False)
     date_id = db.Column(db.Integer, db.ForeignKey("date.id"), nullable=False)
     category = db.Column(db.String(100), nullable=False)
     service = db.Column(db.String(100), nullable=False)
@@ -53,12 +42,8 @@ class Services(db.Model):
 
 class ServiceDate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    org_id = db.Column(
-        db.Integer, db.ForeignKey("organization.id"), nullable=False
-    )
-    service_id = db.Column(
-        db.Integer, db.ForeignKey("services.id"), nullable=False
-    )
+    org_id = db.Column(db.Integer, db.ForeignKey("organization.id"), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
     date = db.Column(db.Date, nullable=False)
     start_time = db.Column(db.Time, nullable=False)
     end_time = db.Column(db.Time, nullable=False)
@@ -76,9 +61,7 @@ class ServiceDate(db.Model):
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    org_id = db.Column(
-        db.Integer, db.ForeignKey("organization.id"), nullable=False
-    )
+    org_id = db.Column(db.Integer, db.ForeignKey("organization.id"), nullable=False)
     street_address = db.Column(db.String(255), nullable=False)
     zip_code = db.Column(db.String(10), nullable=False)
     city = db.Column(db.String(100), nullable=False)
