10a05cbab0948ddac366e8fc91b09b1764d985c9
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-24 23:14:11 -0500

    Adding organization model

--- new_arrivals_chi/app/database.py
+++ new_arrivals_chi/app/database.py
@@ -1,3 +1,20 @@
 """
 Script with the corresponding models for the database.
 """
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Organization(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    location_id = db.Column(
+        db.Integer, db.ForeignKey("location.id"), nullable=False
+    )
+    hours_id = db.Column(db.Integer, db.ForeignKey("hours.id"), nullable=False)
+    phone = db.Column(db.String, nullable=False)
+    image_path = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False)
