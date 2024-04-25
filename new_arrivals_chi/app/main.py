f00d5a0f4d46b6c0a4da86d36449d31862b9f167
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-25 00:45:55 -0500

    Setting up main for first migration..

--- new_arrivals_chi/app/main.py
+++ new_arrivals_chi/app/main.py
@@ -1,6 +1,22 @@
 from flask import Flask, Blueprint, render_template, request
+import os
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 main = Blueprint("main", __name__)
+
+
+load_dotenv()
+app = Flask(__name__)
+app.register_blueprint(main)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @main.route("/")
@@ -14,10 +30,6 @@ def legal():
     language = request.args.get("lang", "en")
     return render_template("legal.html", language=language)
 
-
-app = Flask(__name__)
-
-app.register_blueprint(main)
 
 if __name__ == "__main__":
     app.run(debug=True)
