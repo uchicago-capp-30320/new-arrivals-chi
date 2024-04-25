2888b7e9d889c4e744ec2b8aa5c91adfbcda3ee3
madeleineroberts <C@nney3>
2024-04-25 13:36:41 -0500

    file and method headers added

--- new_arrivals_chi/app/main.py
+++ new_arrivals_chi/app/main.py
@@ -1,3 +1,26 @@
+"""
+Project: new_arrivals_chi
+File name: main.py
+Associated Files:
+    Templates: base.html, home.html, legal.html, profile.html, signup.html, 
+    login.html
+
+Runs primary flask application for Chicago's new arrivals portal.
+
+Methods:
+	* home — Route to homepage of application.
+    * profile - Route to user's profil.
+    * legal - Route to legal portion of application.
+
+Last updated:
+@Author: Madeleine Roberts @MadeleineKRoberts
+@Date: 04/25/2024
+
+Creation:
+@Author: Summer Long @Sumslong
+@Date: 04/19/2024
+"""
+
 from flask import Flask, Blueprint, render_template, request
 
 main = Blueprint("main", __name__)
@@ -9,6 +32,13 @@ def home():
     return render_template("home.html", language=language)
 
 
+"""
+Establishes route for the user's profile page. This route is accessible within 
+the  'profile' button in the navigation bar.
+
+Returns:
+    Renders profile page for user with in their selected language.
+"""
 @main.route("/profile")
 def profile():
     language = request.args.get("lang", "en")
