330cc23060f26a8da9ead801d3dc6eb92b24f377
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-25 20:02:42 -0500

    Adding user table

--- new_arrivals_chi/app/database.py
+++ new_arrivals_chi/app/database.py
@@ -2,6 +2,14 @@ from sqlalchemy import Enum
 from main import db
 
 
+class User(db.Model):
+    id = db.Column(
+        db.Integer, primary_key=True
+    )  # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100), nullable=False)
+
+
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
