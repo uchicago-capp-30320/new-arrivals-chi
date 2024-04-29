577103a0af7b74c036c497723d91691b42143073
Federico Domínguez Molina <fd.molina@outlook.com>
2024-04-29 18:55:20 -0500

    Test modifications

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,12 +6,19 @@ from new_arrivals_chi.app.main import app as flask_app
 @pytest.fixture(scope="module")
 def app():
     """Provides the Flask application instance configured for testing."""
-    flask_app.config["TESTING"] = True
-    flask_app.config["DEBUG"] = False
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     flask_app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    yield flask_app  # Yield the flask_app instance for use in tests
+    flask_app.config["TESTING"] = True
+    flask_app.config["DEBUG"] = False
+    flask_app.config["SERVER_NAME"] = (
+        "localhost.localdomain:5000"  # Example server name
+    )
+    flask_app.config["APPLICATION_ROOT"] = "/"  # Default root
+    flask_app.config["PREFERRED_URL_SCHEME"] = "http"
+
+    with flask_app.app_context():  # Pushes an application context
+        yield flask_app
 
 
 @pytest.fixture(scope="module")
