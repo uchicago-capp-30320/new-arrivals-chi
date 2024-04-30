1385591ee68e7d65c0fc6ea3e3eca1c0d9b72eec
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-04-29 19:34:51 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from new_arrivals_chi.app.main import app as flask_app
 @pytest.fixture(scope="module")
 def app():
     """Provides the Flask application instance configured for testing."""
-    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "")
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     flask_app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     flask_app.config["TESTING"] = True
