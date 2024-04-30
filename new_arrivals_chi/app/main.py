ea9ce8c194d1cd00877d61bbb32046f329042d1c
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-04-29 19:28:55 -0500

    Update main.py

--- new_arrivals_chi/app/main.py
+++ new_arrivals_chi/app/main.py
@@ -91,7 +91,7 @@ def signup():
 app = Flask(__name__)
 app.register_blueprint(main)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "")
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
