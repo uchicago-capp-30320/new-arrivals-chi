9ef3d42056a2e42ceae881efd9e1917fb816c13f
Xiomara Salazar <xiomarasalazar@Xiomaras-Big-MacBook-Pro.local>
2024-05-09 18:52:47 -0500

    Updated setup_fake_db to return test app and db and finalized sql test injections

--- tests/sql_injection_test.py
+++ tests/sql_injection_test.py
@@ -1,30 +1,33 @@
-"""
-Project: New Arrivals Chi
+"""Project: New Arrivals Chi.
+
 File name: sql_injection_test.py
-Associated Files: new_arrivals_chi/app/main.py, new_arrivals_chi/app/logger_config.py, 
-                  tests/db_test.py
+Associated Files: new_arrivals_chi/app/main.py,
+                  new_arrivals_chi/app/logger_config.py, tests/db_test.py.
 
 
-This test suite performs testing on the database to ensure that input handling, 
-particularly regarding SQL injections, is secure and prevents unauthorized database 
-access or manipulation.
+This test suite performs testing on the database to ensure that input handling,
+particularly regarding SQL injections, is secure and prevents unauthorized
+database access or manipulation.
 
 
 Methods:
-    - test_safe_injections: Test function for testing safe input handling or prevention
-                            of SQL injections.
-    - test_unsafe_injections_return_all_rows: Test function for an unsafe input 
-                                              attempting to return all rows from the
-                                              database.
-    - test_unsafe_injections_alter_table: Test function for an unsafe input attempting
-                                          to alter the database table structure.
-    - test_line_comments_injection: Test function for testing prevention of line comments
-                                    in SQL queries.
-    - test_union_injection: Test function for testing prevention of Union-based SQL injections.
-    - test_unsafe_error_based_injection: Test function for testing error-based SQL injections 
-                                         that find column names.
-    - test_unsafe_boolean_based_blind_sql_injection: Test function for simulating an SQL 
-                                                    injection attack using an IF statement.
+    - test_safe_injections: Test function for testing safe input handling or
+                            prevention of SQL injections.
+    - test_unsafe_injections_return_all_rows: Test function for an unsafe input
+                                              attempting to return all rows from
+                                              the database.
+    - test_unsafe_injections_alter_table: Test function for an unsafe input
+                                          attempting to alter the database table
+                                          structure.
+    - test_line_comments_injection: Test function for testing prevention of line
+                                    comments in SQL queries.
+    - test_union_injection: Test function for testing prevention of Union-based
+                            SQL injections.
+    - test_unsafe_error_based_injection: Test function for testing error-based
+                                         SQL injections that find column names.
+    - test_unsafe_boolean_based_blind_sql_injection: Test function for simulat-
+                                                    ing an SQL injection attack
+                                                    using an IF statement.
 
 
 
@@ -42,7 +45,6 @@ from sqlalchemy import select
 from .db_test import create_fake_user, create_fake_organization
 from .setup_fake_db import main
 from new_arrivals_chi.app.database import User
-from new_arrivals_chi.app.main import create_app
 
 
 # creating fake users for queries
@@ -50,13 +52,15 @@ test_org = create_fake_organization()
 test_user = create_fake_user(test_org)
 
 
-def database_query(setup_logger, input_value): 
-    """
-    Function to execute a database query and check the number of rows returned.
+def database_query(setup_logger, input_value):
+    """Executing a database query and check the number of rows returned.
+
+    Executes a database query and checks the number of returned rows based on
+    an input value that filters the email column.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
     test_app, test_db = main()
 
@@ -68,42 +72,47 @@ def database_query(setup_logger, input_value):
 
             session = test_db.session
             logger.info("Attempting to retrieve user with test input")
-            filtered_users_select = select(User).where(User.email == input_value)
+            filtered_users_select = select(User).where(
+                User.email == input_value
+            )
             all_users_select = select(User)
-            
+
             all_users = session.execute(all_users_select).scalars().all()
-            filtered_users = session.execute(filtered_users_select).scalars().all()
+            filtered_users = (
+                session.execute(filtered_users_select).scalars().all()
+            )
 
             logger.info("SQL query was executed")
 
             assert bool(all_users), "No records found in the database."
 
-            logger.info(f"There are records in the database.\n\
-                        Found {len(all_users)} records.")
-            
+            logger.info(
+                f"There are records in the database.\n\
+                        Found {len(all_users)} records."
+            )
+
             metadata = test_db.metadata
-            
+
             test_db.session.remove()
             test_db.drop_all()
             logger.info("Test database removed and test tables dropped.")
-            
+
             return len(filtered_users), metadata
-            
+
     except Exception as e:
         logger.error(f"Error in database query: {e}")
-        raise  # Raise the exception to be caught at a higher level
-
+        raise
 
 
 def test_safe_injections(setup_logger):
-    """
-    Test function for testing safe input handling or prevention of SQL injections.
+    """Testing safe input handling or prevention of SQL injections.
+
+    Ensure safe input to SQL database does not return any records. This is a
+    fake user so it shouldn't be in the database.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
-
-    Return: Asserts that fake user isn't in test 'user' table.
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
     input_value = f"'{test_user.email}'"
 
@@ -113,14 +122,17 @@ def test_safe_injections(setup_logger):
 
 
 def test_unsafe_injection_return_all_rows(setup_logger):
-    """
-    Test function for an unsafe input attempting to return all rows.
+    """Testing for an unsafe input attempting to return all rows.
+
+    Verifying safe input handling or prevention of SQL injections by querying
+    the database and checking the number of rows returned.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = f"'{test_user.email}' OR 1=1;"
+    input_value = f"'{test_user.email}'"
+    input_value += " OR 1=1; "
 
     row_count, metadata = database_query(setup_logger, input_value)
 
@@ -128,15 +140,19 @@ def test_unsafe_injection_return_all_rows(setup_logger):
 
 
 def test_unsafe_injection_alter_table(setup_logger):
-    """
-    Test function for an unsafe input attempting to alter the table.
-    Checks if table still exists and if anything is returned.
+    """Testing for an unsafe input attempting to alter the table.
+
+    Ensures that an unsafe input does not alter the table structure
+    using an SQL injection. It checks if the specified table still exists and
+    if any rows are returned after the query.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = f"'{test_user.email}'; DROP TABLE users; --"
+    input_value = f"'{test_user.email}'"
+    input_value += "; DROP TABLE users; -- "
+
     row_count, metadata = database_query(setup_logger, input_value)
 
     # Check if the 'user' table exists in the database metadata
@@ -147,14 +163,17 @@ def test_unsafe_injection_alter_table(setup_logger):
 
 
 def test_unsafe_line_comments_injection(setup_logger):
-    """
-    Test function for testing prevention of line comments in SQL queries.
+    """Testing prevention of line comments in SQL queries.
+
+    Ensures the prevention of line comments in SQL queries. It checks if the
+    specified table still exists and if any rows are returned after the query.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = f"'{test_user.email}'; DROP TABLE users /*"
+    input_value = f"'{test_user.email}'"
+    input_value += "; DROP TABLE users /*"
     row_count, metadata = database_query(setup_logger, input_value)
 
     # Check if the 'user' table exists in the database metadata
@@ -165,14 +184,17 @@ def test_unsafe_line_comments_injection(setup_logger):
 
 
 def test_unsafe_union_injection(setup_logger):
-    """
-    Test function for testing prevention of Union-based SQL injections.
+    """Testing prevention of Union-based SQL injections.
+
+    Ensures the prevention of Union-based SQL injections. It checks if if any
+    rows are returned after the query.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = f"{test_user.email} UNION SELECT * FROM organization;"
+    input_value = f"'{test_user.email}' "
+    input_value += "UNION SELECT * FROM organization; "
 
     row_count, metadata = database_query(setup_logger, input_value)
 
@@ -180,15 +202,18 @@ def test_unsafe_union_injection(setup_logger):
 
 
 def test_unsafe_error_based_injection(setup_logger):
-    """
-    Test function for testing error-based SQL injections that find column names.
+    """Testing error-based SQL injections that find column names.
+
+    Ensures that an error-based SQL injections does not find column names.
+    It checks if any rows are returned after the query.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = f"'{test_user.email}' HAVING 1=1 UNION SELECT 1, \
-                    group_concat(name) FROM sqlite_master WHERE type='table';"
+    input_value = f"'{test_user.email}'"
+    input_value += "HAVING 1=1 UNION SELECT 1, \
+                   group_concat(name) FROM sqlite_master WHERE type='table'; "
 
     row_count, metadata = database_query(setup_logger, input_value)
 
@@ -196,16 +221,19 @@ def test_unsafe_error_based_injection(setup_logger):
 
 
 def test_unsafe_if_statement(setup_logger):
-    """
-    Test function for simulating an SQL injection attack using an IF statement.
+    """Testings an SQL injection attack using an IF statement.
+
+    Ensuring that an SQL injection attack using an IF statement does not return
+    all rows. It checks if any rows are returned after the query.
 
     Args:
-    - setup_logger: Setup logger.
-    - input_value: Input value for the database query that filters on email
+        setup_logger: Setup logger.
+        input_value: Input value for the database query that filters on email
     """
-    input_value = (
-        f"{test_user.email}' OR 1=1; IF 1=1 BEGIN SELECT 1 ELSE SELECT 0 END --"
-    )
+    input_value = f"'{test_user.email}'"
+    input_value += "OR "
+    input_value += "1=1; "
+    input_value += "IF 1=1 BEGIN SELECT 1 ELSE SELECT 0 END --"
 
     row_count, metadata = database_query(setup_logger, input_value)
 
