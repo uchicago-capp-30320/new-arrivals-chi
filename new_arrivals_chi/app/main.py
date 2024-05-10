622876d56284c55345810162f831ad9cf3b1fdf7
Kathryn Link-Oberstar <111700741+klinkoberstar@users.noreply.github.com>
2024-05-09 23:31:02 -0500

    Feat mitigating xss attacks (#162)
    This PR will close #146 by sanitizing all user inputs and encoding
    user-controlled data that is outputed in HTTP responses
    
    ## Describe your changes
    
    Sanitization of User Inputs:
    - Implemented bleach.clean() to sanitize all user inputs across the
    application. Removes potentially malicious scripts from the inputs
    before they are processed or stored. All form inputs, including login
    and password change forms, now pass through bleach.clean() before
    processing. Query parameters are sanitized where they are used in the
    application logic.
    
    Encoding of User-Controlled Data in Templates:
    - Utilized the | escape Jinja2 filter in our HTML templates to ensure
    that any user-controlled data is safely encoded before being rendered in
    the browser.
    
    - Applied escape() function in conjunction with Flask's flash() messages
    to encode feedback messages before they are displayed to the user.
    
    ## Non-obvious technical information
    - the | escape Jinja2 filter is redundant, the template does this by
    default so we can remove if we feel like it is unnecessary
    
    ## Checklist before requesting a review
    - [X] pre-commit run --all-files (run before pushing)
    - [X] pytest if applicable
    - [X] Link issue
    - [X] Update relevant documentation if applicable: doc strings, readme,
    poetry.
    
    ```commandline
    HERE IS SOME COMMAND LINE OUTPUT
    ```

--- new_arrivals_chi/app/main.py
+++ new_arrivals_chi/app/main.py
@@ -23,6 +23,7 @@ Creation:
 
 from flask import Flask, Blueprint, render_template, request
 import os
+import bleach
 from dotenv import load_dotenv
 from new_arrivals_chi.app.database import db, User
 from flask_migrate import Migrate
@@ -47,7 +48,7 @@ def home():
     Returns:
         Renders home page.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("home.html", language=language)
 
 
@@ -61,7 +62,7 @@ def profile():
     Returns:
         Renders profile page for user with in their selected language.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("profile.html", language=language)
 
 
@@ -74,7 +75,7 @@ def legal():
     Returns:
         Renders main legal page.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("legal.html", language=language)
 
 
@@ -87,7 +88,7 @@ def health():
     Returns:
         Renders main health page.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("health.html", language=language)
 
 
@@ -101,7 +102,7 @@ def health_search():
     Returns:
         Renders the health search page.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("health_search.html", language=language)
 
 
@@ -114,7 +115,7 @@ def info():
     Returns:
         Renders information of an organization.
     """
-    language = request.args.get("lang", "en")
+    language = bleach.clean(request.args.get("lang", "en"))
     return render_template("info.html", language=language)
 
 
